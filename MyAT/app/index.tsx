021971e5f6edb012bf5a5d26d6c17ec2a8ad86ca
Erkin Khoroshi <calculus.antiderivative@gmail.com>
2024-12-23 21:06:49 -0500

    Created Login UI

--- MyAT/app/index.tsx
+++ MyAT/app/index.tsx
@@ -1,15 +1,80 @@
-import { Text, View } from "react-native";
+import React, { useState} from 'react';
+import { View, Text, StyleSheet, Alert, TextInput, TouchableOpacity } from 'react-native';
 
-export default function Index() {
+const app = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>Augusta Transportation</Text>
+
+        <Text style={styles.subheading}>Sign-In</Text>
+
+        <Text style={styles.subtext}>Email:</Text>
+        <TextInput style={styles.input} placeholder='Email'></TextInput>
+
+        <Text style={styles.subtext}>Password:</Text>  
+        <TextInput style={styles.input} placeholder='Password'></TextInput>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
     </View>
-  );
+      
+     
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
+
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 24, 
+    marginTop: 150,
+  },
+
+  subheading: {
+    textAlign: 'center',
+    fontSize: 24,
+  },
+
+  subtext: {
+    fontSize: 24,
+    marginVertical: 20, 
+  },
+
+  input: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+
+  button: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#007BFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    marginTop: 50,
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+}
+)
+
+export default app;
